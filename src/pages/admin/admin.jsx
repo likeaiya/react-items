3d7284082dad5f8b8b4725d9c17ce6a649a80cc2
likeaiya <1298804523@qq.com>
2020-02-10 21:49:57 +0800

    更新

---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class admin extends Component {
+    render() {
+        return (
+            <h1>
+                admin
+            </h1>
+        )
+    }
+}
+
