6e302f8ce5de19cead7e37838db86842fd7e2b4d
likeaiya <1298804523@qq.com>
2020-02-15 01:12:26 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Route,Switch} from 'react-router-dom'
 
 export default class App extends Component {
     render() {
+      
         return (
           <Switch>
             <Route path="/login" component={Login}/>
